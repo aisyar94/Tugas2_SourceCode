8d0e7cd921ca0a9c303ea76ba9d52c551809b3e4
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-24 03:52:55 -0700

    Create ForEver.java

---
+++ ForEver.java
@@ -0,0 +1,22 @@
+/*Nama: Aisyah Safira Rachman*/
+/*NIM: 13020210004*/
+/*Hari/Tanggal: Jumaat/24-03-2003*/
+/*Waktu: 4.15 PM*/
+package tgs2;
+
+public class ForEver {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true){
+            System.out.print ("Print satu baris ....\n");
+        }
+    
+    }
+    
+}
