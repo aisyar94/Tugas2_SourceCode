3c2a9619685616e73baba8df43c4845848c69231
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-24 04:07:09 -0700

    Create PrintWhile.java

---
+++ PrintWhile.java
@@ -0,0 +1,34 @@
+/*Nama: Aisyah Safira Rachman*/
+/*NIM: 13020210004*/
+/*Hari/Tanggal: Jumaat/24-03-2003*/
+/*Waktu: 7.11 PM*/
+package tgs13;
+import java.util.Scanner;
+
+/* Baca N, Print i = 1 s/d N dengan WHILE */
+public class PrintWhile {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus : */
+        int N;
+        int i;
+        Scanner masukan=new Scanner(System.in);
+        /* Program */
+
+        System.out.print ("Nilai N > 0 = "); /* Inisialisasi */
+        N = masukan.nextInt();
+        i = 1;  /* First Elmt */
+
+        System.out.print ("Print i dengan WHILE: \n");
+        while (i <= N)   /* Kondisi pengulangan */ {
+            System.out.println (i);     /* Proses */
+            i++;    /* Next Elmt */
+        };          /* (i > N) */
+        
+    }
+    
+}
