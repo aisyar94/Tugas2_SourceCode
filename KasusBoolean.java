a1fc75bbcec4d7647fbcb41fa25c3c63d0e39b77
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-24 03:58:58 -0700

    Create KasusBoolean.java

---
+++ KasusBoolean.java
@@ -0,0 +1,29 @@
+/*Nama: Aisyah Safira Rachman*/
+/*NIM: 13020210004*/
+/*Hari/Tanggal: Jumaat/24-03-2003*/
+/*Waktu: 5.07 PM*/
+package tgs6;
+
+/* Eksrpesi kondisional dengan boolean */
+public class KasusBoolean {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        boolean bool;
+        /* Algoritma */
+        bool = true;
+        if(bool) {
+            System.out.print("true\n");
+        } else
+            System.out.print("false\n");
+        if(!bool) {
+            System.out.print("salah\n");
+        } else
+            System.out.print("benar\n");
+
+    } 
+}
